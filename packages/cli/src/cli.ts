aece6d305c7396551811def95d601512a88b5f03
Jonas Birmé <jonas.birme@eyevinn.se>
2024-05-28 13:34:35 +0200

    feat: transcode command in CLI (#7)

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -3,6 +3,7 @@
 import { Command } from 'commander';
 import cmdAdmin from './admin/cmd';
 import * as cmdUser from './user/cmd';
+import cmdTranscode from './transcode/cmd';
 
 const cli = new Command();
 
@@ -13,4 +14,5 @@ cli.addCommand(cmdAdmin());
 cli.addCommand(cmdUser.cmdList());
 cli.addCommand(cmdUser.cmdCreate());
 cli.addCommand(cmdUser.cmdRemove());
+cli.addCommand(cmdTranscode());
 cli.parse(process.argv);
