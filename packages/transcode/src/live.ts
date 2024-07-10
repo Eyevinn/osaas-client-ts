2d19ea90bcbf5f35538580c64d7ea0e6a9f8f105
Jonas Birmé <jonas.birme@eyevinn.se>
2024-07-11 00:05:10 +0200

    feat: live multi-bitrate hls encoding with eyevinn live encoding (#30)

--- packages/transcode/src/live.ts
+++ packages/transcode/src/live.ts
@@ -1,10 +1,121 @@
 import {
   Context,
+  Log,
+  createFetch,
   createInstance,
+  getInstance,
   getPortsForInstance,
   listInstances,
   removeInstance
 } from '@osaas/client-core';
+import { delay } from './util';
+
+export async function createLiveMultiBitrateHLS(
+  ctx: Context,
+  name: string,
+  streamKey?: string
+): Promise<{ rtmpUrl: URL; hlsUrl: URL }> {
+  const serviceAccessToken = await ctx.getServiceAccessToken(
+    'eyevinn-live-encoding'
+  );
+
+  await createInstance(ctx, 'eyevinn-live-encoding', serviceAccessToken, {
+    name,
+    HlsOnly: true,
+    StreamKey: streamKey
+  });
+  await delay(2000);
+
+  const encoding = await startLiveMultiBitrateHLS(ctx, name);
+  await delay(1000);
+  if (!encoding) {
+    throw new Error('Failed to start live encoding');
+  }
+  return {
+    rtmpUrl: encoding.rtmpUrl,
+    hlsUrl: encoding.hlsUrl
+  };
+}
+
+export async function startLiveMultiBitrateHLS(
+  ctx: Context,
+  name: string
+): Promise<{ rtmpUrl: URL; hlsUrl: URL }> {
+  const serviceAccessToken = await ctx.getServiceAccessToken(
+    'eyevinn-live-encoding'
+  );
+  const encoder = await getInstance(
+    ctx,
+    'eyevinn-live-encoding',
+    name,
+    serviceAccessToken
+  );
+  if (encoder) {
+    const ports = await getPortsForInstance(
+      ctx,
+      'eyevinn-live-encoding',
+      encoder.name,
+      serviceAccessToken
+    );
+    const rtmpPort = ports.find((port) => port.internalPort == 1935);
+    if (!rtmpPort) {
+      throw new Error('Instance has no RTMP port mapped');
+    }
+
+    const encoding = await createFetch<{ streamKey: string; playlist: string }>(
+      new URL(encoder.url + '/encoder'),
+      {
+        method: 'POST',
+        headers: {
+          'x-jwt': `Bearer ${serviceAccessToken}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ timeout: 0 })
+      }
+    );
+    await delay(5000);
+    const status = await createFetch<any>(new URL(encoder.url + '/encoder'), {
+      method: 'GET',
+      headers: {
+        'x-jwt': `Bearer ${serviceAccessToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    Log().debug(status);
+    if (status.status != 'starting') {
+      throw new Error('Failed to start encoder');
+    }
+    return {
+      rtmpUrl: new URL(
+        `rtmp://${rtmpPort.externalIp}:${rtmpPort.externalPort}/live/${encoding.streamKey}`
+      ),
+      hlsUrl: new URL(encoder.url + encoding.playlist)
+    };
+  } else {
+    throw new Error('Encoder not found');
+  }
+}
+
+export async function stopLiveMultiBitrateHLS(ctx: Context, name: string) {
+  const serviceAccessToken = await ctx.getServiceAccessToken(
+    'eyevinn-live-encoding'
+  );
+  const encoder = await getInstance(
+    ctx,
+    'eyevinn-live-encoding',
+    name,
+    serviceAccessToken
+  );
+  if (encoder) {
+    await createFetch<any>(new URL(encoder.url + '/encoder'), {
+      method: 'DELETE',
+      headers: {
+        'x-jwt': `Bearer ${serviceAccessToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+}
 
 export async function createLiveSingleBitrateHLS(
   ctx: Context,
