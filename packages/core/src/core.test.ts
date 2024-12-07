debd496d28a73a67dc6d3e033d8e01f585148032
Jonas Birmé <jonas.birme@eyevinn.se>
2024-12-07 20:36:36 +0100

    fix: check instance name is valid

---
+++ packages/core/src/core.test.ts
@@ -0,0 +1,24 @@
+import { Context } from './context';
+import { createInstance, isValidInstanceName } from './core';
+import { InvalidName } from './errors';
+
+jest.mock('./fetch');
+
+describe('Core functionalities', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test('validate instance name', () => {
+    expect(isValidInstanceName('myinstance')).toEqual(true);
+    expect(isValidInstanceName('my-instance')).toEqual(false);
+  });
+
+  test('an instance with an invalid name cannot be created', async () => {
+    await expect(
+      createInstance(new Context(), 'eyevinn-test-adserver', 'my-token', {
+        name: 'my-instance'
+      })
+    ).rejects.toThrow(new InvalidName('my-instance'));
+  });
+});
