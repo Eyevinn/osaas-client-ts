bd1480c4e2a6e426df67dae094bec606967e8c3b
Jonas Birmé <jonas.birme@eyevinn.se>
2024-12-07 21:58:33 +0100

    fix: test that valid instance names can be used

--- packages/core/src/core.test.ts
+++ packages/core/src/core.test.ts
@@ -1,6 +1,7 @@
 import { Context } from './context';
 import { createInstance, isValidInstanceName } from './core';
 import { InvalidName } from './errors';
+import { createFetch } from './fetch';
 
 jest.mock('./fetch');
 
@@ -25,5 +26,20 @@ describe('Core functionalities', () => {
         }
       )
     ).rejects.toThrow(new InvalidName('my-instance'));
+    expect(createFetch).not.toHaveBeenCalled();
+  });
+
+  test('an instance with an valid name can be created', async () => {
+    await expect(
+      createInstance(
+        new Context({ personalAccessToken: 'dummy' }),
+        'eyevinn-test-adserver',
+        'my-token',
+        {
+          name: 'myinstance'
+        }
+      )
+    ).rejects.not.toThrow(new InvalidName('myinstance'));
+    expect(createFetch).toHaveBeenCalled();
   });
 });
